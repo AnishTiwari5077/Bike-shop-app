cce7badcbf409900485a27ba02346b855a4daae7
Darkangel121212 <anishtiwari5077@gmail.com>
2026-06-01 09:01:32 +0545

    updated

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -2,5 +2,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConfig {
   static String get baseUrl =>
-      dotenv.env['API_BASE_URL'] ?? 'http://192.168.1.15:3000';
+      dotenv.env['API_BASE_URL'] ?? 'http://localhost:3000';
 }
